38e75559af0dbe5abd9df80e8bdab65a952656df
Heather Barr <hnbarr@yahoo.com>
2019-06-04 23:03:10 -0500

    progress made about half way done now

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
+const state = require('./state')
+
+
+app.get('/users', (req, res) => {res.send(state.users)})
+
+
+app.get('/users/1', (req, res) => {res.json(state.users[0])})
+
+
+app.post('/users', (req, res) => {
+  state.users.push({ name: "Fox Mulder" });
+  res.json(state.users[state.users.length - 1]); //returning last item, new item
+})
+
+app.put('/users/1', (req, res) => {res.json(state.users[0])})
+
+
+app.delete('/users', (req, res) => {
+  state.users.shift();
+  res.send('deleted') //returning that user was deleted
+})
+
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))

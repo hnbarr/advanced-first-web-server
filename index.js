eb4fbfd7676d2ccbcf693dcfa2beb32b893dd580
Heather Barr <hnbarr@yahoo.com>
2019-06-04 23:32:59 -0500

    Stoppping for now. Done but still want to review and study a little more on this one.

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000 
 const state = require('./state')
 
+app.use(bodyParser.json());
+let lastId = state.users[state.users.length - 1]._id;
 
-app.get('/users', (req, res) => {res.send(state.users)})
-
-
-app.get('/users/1', (req, res) => {res.json(state.users[0])})
-
+app.get('/users', (req, res) => {res.send(state.users)}) //working, returning all users
+app.get('/users/:userId', (req, res) => {
+  const reqUser = state.users.find(user => user._id == req.params.userId);
+  reqUser && res.send(reqUser); //if true, res.send
+}) //working, returning only user I ask for
 
 app.post('/users', (req, res) => {
-  state.users.push({ name: "Fox Mulder" });
-  res.json(state.users[state.users.length - 1]); //returning last item, new item
+  const newUser = req.body;
+  lastId++;
+  newUser._id = lastId;
+  state.users.push(newUser);
+  res.json(newUser); 
 })
 
-app.put('/users/1', (req, res) => {res.json(state.users[0])})
+app.put('/users/:userId', (req, res) => {
+  const userIndex = state.users.findIndex(
+    user => user._id == req.params.userId
+  );
+  if (userIndex > -1) {
+    state.users[userIndex].occupation = "prosk8er";
+    res.json(state.users[userIndex]);
+  } else {
+    res.send("not a valid userId");
+  }
+}) //working, changing the occupation to any user I select.
 
-
-app.delete('/users', (req, res) => {
-  state.users.shift();
-  res.send('deleted') //returning that user was deleted
-})
-
+app.delete('/users/:userId', (req, res) => {
+  const userIndex = state.users.findIndex(
+    user => user._id == req.params.userId
+  );
+  if (userIndex > -1) {
+    state.users[userIndex].isActive = false;
+    res.send(`deleted`);
+  } else {
+    res.send("not a valid userId");
+  }
+}) // working, deleting and returning deleted.
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))
